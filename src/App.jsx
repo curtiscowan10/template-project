f6041ce23e2d6daffca1f013da67f4d448fd365f
Curtis Cowan <curtiscowan10@gmail.com>
2022-10-28 01:10:29 +0100

    Pre commit test

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  $unusedConst = 1;
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-         Template Project
+                                      Template Project
                 </p>
             </header>
         </div>

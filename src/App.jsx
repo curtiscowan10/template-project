9b740e22013b64194e23b5950a15764c511e5456
Curtis Cowan <curtiscowan10@gmail.com>
2022-10-28 12:35:59 +0100

    Pre commit test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+
+
+
+
 import "./App.css";
 
+import logo from "./logo.svg";
+
+
 function App() {
-  $unusedConst = 1;
+    $unusedConst = 1;
     return (
         <div className="App">
             <header className="App-header">

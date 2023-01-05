d0f007c3bbe5709a34e61e6d768e9bf5ea7231b7
Curtis Cowan <curtiscowan10@gmail.com>
2023-01-05 19:48:56 +0000

    Failing unit test due to text change

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders template project text", () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText("Template Project");
     expect(linkElement).toBeInTheDocument();
 });
